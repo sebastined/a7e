8e4de15daa77d2873dacf6a0d78fdbed689c1a74
sebastined <72633214+sebastined@users.noreply.github.com>
2025-03-15 12:26:11 +0100

    ff

--- vscode/sre/Aurora/Alpha/main.tf
+++ vscode/sre/Aurora/Alpha/main.tf
@@ -4,85 +4,85 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-resource "aws_vpc" "aur_vpc" {
+resource "aws_vpc" "omega_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
-    Name = "aur-vpc"
+    Name = "omega-vpc"
   }
 }
 
-resource "aws_subnet" "aur_public_1" {
-  vpc_id                  = aws_vpc.aur_vpc.id
+resource "aws_subnet" "omega_pub_1" {
+  vpc_id                  = aws_vpc.omega_vpc.id
   cidr_block              = "10.0.0.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "eu-central-1a"
   tags = {
-    Name = "aur-public-subnet-1"
+    Name = "omega-public-subnet-1"
   }
 }
 
-resource "aws_subnet" "aur_public_2" {
-  vpc_id                  = aws_vpc.aur_vpc.id
+resource "aws_subnet" "omega_pub_2" {
+  vpc_id                  = aws_vpc.omega_vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "eu-central-1b"
   tags = {
-    Name = "aur-public-subnet-2"
+    Name = "omega-public-subnet-2"
   }
 }
 
-resource "aws_subnet" "aur_private_1" {
-  vpc_id            = aws_vpc.aur_vpc.id
+resource "aws_subnet" "omega_pri_1" {
+  vpc_id            = aws_vpc.omega_vpc.id
   cidr_block        = "10.0.253.0/24"
   availability_zone = "eu-central-1a"
   tags = {
-    Name = "aur-private-subnet-1"
+    Name = "omega-private-subnet-1"
   }
 }
 
-resource "aws_subnet" "aur_private_2" {
-  vpc_id            = aws_vpc.aur_vpc.id
+resource "aws_subnet" "omega_pri_2" {
+  vpc_id            = aws_vpc.omega_vpc.id
   cidr_block        = "10.0.254.0/24"
   availability_zone = "eu-central-1b"
   tags = {
-    Name = "aur-private-subnet-2"
+    Name = "omega-private-subnet-2"
   }
 }
 
-resource "aws_internet_gateway" "aur_igw" {
-  vpc_id = aws_vpc.aur_vpc.id
+resource "aws_internet_gateway" "omega_igw" {
+  vpc_id = aws_vpc.omega_vpc.id
   tags = {
-    Name = "aur-igw"
+    Name = "omega-igw"
   }
 }
 
-resource "aws_route_table" "aur_rt" {
-  vpc_id = aws_vpc.aur_vpc.id
+resource "aws_route_table" "omega_rt" {
+  vpc_id = aws_vpc.omega_vpc.id
   tags = {
-    Name = "aur-public-route-table"
+    Name = "omega-public-route-table"
   }
 }
 
-resource "aws_route" "aur_internet_access" {
-  route_table_id         = aws_route_table.aur_rt.id
+resource "aws_route" "omega_int_access" {
+  route_table_id         = aws_route_table.omega_rt.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.aur_igw.id
+  gateway_id             = aws_internet_gateway.omega_igw.id
 }
 
-resource "aws_route_table_association" "aur_public_assoc_1" {
-  subnet_id      = aws_subnet.aur_public_1.id
-  route_table_id = aws_route_table.aur_rt.id
+resource "aws_route_table_association" "omega_pub_1" {
+  subnet_id      = aws_subnet.omega_public_1.id
+  route_table_id = aws_route_table.omega_rt.id
 }
 
-resource "aws_route_table_association" "aur_public_assoc_2" {
-  subnet_id      = aws_subnet.aur_public_2.id
-  route_table_id = aws_route_table.aur_rt.id
+resource "aws_route_table_association" "omega_pub_2" {
+  subnet_id      = aws_subnet.omega_public_2.id
+  route_table_id = aws_route_table.omega_rt.id
 }
 
-resource "aws_security_group" "aur_sg" {
-  vpc_id = aws_vpc.aur_vpc.id
+resource "aws_security_group" "omega_sg" {
+  vpc_id = aws_vpc.omega_vpc.id
 
   ingress {
     from_port   = 22
@@ -113,17 +113,17 @@ resource "aws_security_group" "aur_sg" {
   }
 
   tags = {
-    Name = "aur-sg"
+    Name = "omega-sg"
   }
 }
 
-resource "aws_instance" "aur_web" {
+resource "aws_instance" "omega_web" {
   ami                    = "ami-07eef52105e8a2059"
   instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.aur_public_1.id
+  subnet_id              = aws_subnet.omega_public_1.id
   key_name               = "nixssh"
-  vpc_security_group_ids = [aws_security_group.aur_sg.id]
+  vpc_security_group_ids = [aws_security_group.omega_sg.id]
   tags = {
-    Name = "aur-web-instance"
+    Name = "omega-web-instance"
   }
 }
