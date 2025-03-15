bd2051bdc57fa69203286bb49e6bab8269a8d1db
sebastined <72633214+sebastined@users.noreply.github.com>
2025-03-15 13:19:37 +0100

    dd

--- vscode/sre/Aurora/Alpha/main.tf
+++ vscode/sre/Aurora/Alpha/main.tf
@@ -1,5 +1,3 @@
-#source=zero_VM
-
 provider "aws" {
   region = "eu-central-1"
 }
@@ -13,41 +11,41 @@ resource "aws_vpc" "omega_vpc" {
   }
 }
 
-resource "aws_subnet" "omega_pub_1" {
+resource "aws_subnet" "omega_pub1" {
   vpc_id                  = aws_vpc.omega_vpc.id
   cidr_block              = "10.0.0.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "eu-central-1a"
   tags = {
-    Name = "omega-public-subnet-1"
+    Name = "omega-pub1"
   }
 }
 
-resource "aws_subnet" "omega_pub_2" {
+resource "aws_subnet" "omega_pub2" {
   vpc_id                  = aws_vpc.omega_vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "eu-central-1b"
   tags = {
-    Name = "omega-public-subnet-2"
+    Name = "omega-pub2"
   }
 }
 
-resource "aws_subnet" "omega_pri_1" {
+resource "aws_subnet" "omega_priv1" {
   vpc_id            = aws_vpc.omega_vpc.id
   cidr_block        = "10.0.253.0/24"
   availability_zone = "eu-central-1a"
   tags = {
-    Name = "omega-private-subnet-1"
+    Name = "omega-priv1"
   }
 }
 
-resource "aws_subnet" "omega_pri_2" {
+resource "aws_subnet" "omega_priv2" {
   vpc_id            = aws_vpc.omega_vpc.id
   cidr_block        = "10.0.254.0/24"
   availability_zone = "eu-central-1b"
   tags = {
-    Name = "omega-private-subnet-2"
+    Name = "omega-priv2"
   }
 }
 
@@ -61,23 +59,23 @@ resource "aws_internet_gateway" "omega_igw" {
 resource "aws_route_table" "omega_rt" {
   vpc_id = aws_vpc.omega_vpc.id
   tags = {
-    Name = "omega-public-route-table"
+    Name = "omega-rt"
   }
 }
 
-resource "aws_route" "omega_int_access" {
+resource "aws_route" "omega_inet" {
   route_table_id         = aws_route_table.omega_rt.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.omega_igw.id
 }
 
-resource "aws_route_table_association" "omega_pub_1" {
-  subnet_id      = aws_subnet.omega_public_1.id
+resource "aws_route_table_association" "omega_pub_assoc1" {
+  subnet_id      = aws_subnet.omega_pub1.id
   route_table_id = aws_route_table.omega_rt.id
 }
 
-resource "aws_route_table_association" "omega_pub_2" {
-  subnet_id      = aws_subnet.omega_public_2.id
+resource "aws_route_table_association" "omega_pub_assoc2" {
+  subnet_id      = aws_subnet.omega_pub2.id
   route_table_id = aws_route_table.omega_rt.id
 }
 
@@ -120,10 +118,10 @@ resource "aws_security_group" "omega_sg" {
 resource "aws_instance" "omega_web" {
   ami                    = "ami-07eef52105e8a2059"
   instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.omega_public_1.id
+  subnet_id              = aws_subnet.omega_pub1.id
   key_name               = "nixssh"
   vpc_security_group_ids = [aws_security_group.omega_sg.id]
   tags = {
-    Name = "omega-web-instance"
+    Name = "omega-web"
   }
 }
